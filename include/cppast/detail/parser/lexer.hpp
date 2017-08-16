f3f69e5ac31fd23116cae14e74bb1054cc6aa2f6
Manu343726 <Manu343726@gmail.com>
2017-08-16 17:36:40 +0200

    lexer docs

--- include/cppast/detail/parser/lexer.hpp
+++ include/cppast/detail/parser/lexer.hpp
@@ -17,17 +17,59 @@ namespace detail
 namespace parser
 {
 
+/// Returns a token stream out of some parsing input
+///
+/// lexer provides an interface to tokenize some input and
+/// return the resulting tokens. The interface works as a live
+/// scanner where a new (the next) token from the input is returned
+/// on demand, by invoking `read_next_token()` function. If `read_next_token()`
+/// successfuly read a new token, returns true and puts the new token
+/// accessible through `current_token()` function. When no more tokens
+/// are available (Or reading the next token resulted in a lexing error)
+/// `read_next_token()` returns false. Invoking `current_token()` before
+/// invoking `read_next_token()` or after `read_next_token()` returned false
+/// has undefined behavior.
+///
+/// In general, the usage of `current_token()` and `read_next_token()` after
+/// a `false` return from `read_next_token()` has undefined behavior.
+///
+/// The interface is designed with a "fetch tokens loop" in mind:
+///
+/// ``` cpp
+/// while(lexer.read_next_token())
+/// {
+///     do_something(lexer.current_token());
+/// }
+///
+/// See `lexer_example.cpp` for a full example.
 class lexer
 {
 public:
     virtual ~lexer() = default;
 
+    /// Scans the next token from the input
+    /// \returns true if a new token was read from the input. False if
+    /// an error occurred or if there was no more input available.
+    /// \notes When returning true on a successful read, the read token
+    /// becomes accessible through `current_token()`.
     virtual bool read_next_token() = 0;
+
+    /// Returns the last token read by the lexer, if any
     virtual const token& current_token() const = 0;
+
+    /// Checks if the lexer can continue reading tokens
+    /// \returns true if the lexer can continue scanning, false otherwise
     virtual bool good() const = 0;
+
+    /// Checks if the lexer reached the end of input
     virtual bool eof() const = 0;
+
+    /// Returns the source location in the input the lexer is currently
+    /// pointing to.
+    /// \notes It's equivalent to the source location of the current token.
     virtual source_location location() const = 0;
 
+    /// Returns the diagnostic logger being used by the lexe.
     const diagnostic_logger& logger() const;
 
 protected:
