195d1c3407bbe79ec06cd31408428148273a73b1
Manu343726 <Manu343726@gmail.com>
2017-08-16 23:41:20 +0200

    fake lexer docs

--- include/cppast/detail/parser/fake_lexer.hpp
+++ include/cppast/detail/parser/fake_lexer.hpp
@@ -17,19 +17,40 @@ namespace detail
 namespace parser
 {
 
+/// Implements an interface to a sequence of already lexed tokens
+///
+/// Fake lexer provides a way to feed a parser with an existing sequence
+/// of tokens, such as tokens taken directly from the C++ AST
 class fake_lexer : public lexer
 {
 public:
+    /// Initializes a fake lexer with the sequence of tokens to "read" and
+    /// a diagnostic logger for the lexer.
     fake_lexer(const std::vector<token>& tokens, const diagnostic_logger& logger);
 
+    /// Reads the next token from the secuence and puts it available to
+    /// read at `current_token()`.
+    /// \returns True if a token was read from the sequence, false if there
+    /// are no more tokens in the sequence.
     bool read_next_token() override;
+
+    /// Returns the token the sequence the lexer is currently pointing
+    /// to. The behavior is undefined if the function is called when the lexer
+    /// has already consumed the full sequence.
     const token& current_token() const override;
 
+    /// Checks if the lexer can continue ready¡ing tokens.
     bool good() const override;
+
+    /// Checks if the lexer has reached the end of the sequence.
+    /// \notes Equivalent to `good()` ?
     bool eof() const override;
 
+    /// Returns the source location of the token the lexer is currently
+    /// pointing to.
     source_location location() const override;
 
+    /// Returns the full sequence of tokens
     const std::vector<token>& tokens() const;
 
 private:
