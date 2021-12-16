e90af5bd6f21b2741a16fd63f354ffbbb3f70a93
jackalcooper <jackalcooper@gmail.com>
2021-12-16 08:34:03 +0000

    refine

--- example/example_parser.hpp
+++ example/example_parser.hpp
@@ -5,15 +5,17 @@
 #ifndef CPPAST_EXAMPLE_PARSER_HPP_INCLUDED
 #define CPPAST_EXAMPLE_PARSER_HPP_INCLUDED
 
-#include <iostream>
-
 #include <cppast/libclang_parser.hpp>
+#include <fstream>
+#include <iostream>
+#include <string>
 
 // reads the database directory from the command line argument
 // parses all files in that directory
 // and invokes the callback for each of them
 template <typename Callback>
-int example_main(int argc, char* argv[], const cppast::cpp_entity_index& index, Callback cb) try
+int example_main(int argc, char* argv[], const cppast::cpp_entity_index& index, Callback cb)
+try
 {
     if (argc != 2)
     {
@@ -25,10 +27,41 @@ int example_main(int argc, char* argv[], const cppast::cpp_entity_index& index, 
         cppast::libclang_compilation_database database(argv[1]); // the compilation database
 
         // simple_file_parser allows parsing multiple files and stores the results for us
-        cppast::simple_file_parser<cppast::libclang_parser> parser(type_safe::ref(index));
+        using ParserT = cppast::simple_file_parser<cppast::libclang_parser>;
+        ParserT                  parser(type_safe::ref(index));
+        static const std::string database_dir = std::string(argv[1]);
         try
         {
-            cppast::parse_database(parser, database); // parse all files in the database
+            using namespace cppast;
+            struct data_t
+            {
+                ParserT&                             parser;
+                const libclang_compilation_database& database;
+            } data{parser, database};
+            detail::for_each_file(database, &data, [](void* ptr, std::string file) {
+                auto&      data               = *static_cast<data_t*>(ptr);
+                const bool is_cpp             = file.find(".cpp") != std::string::npos;
+                const bool is_in_database_dir = file.find(database_dir) != std::string::npos;
+                const bool is_in_user_op_dir  = file.find("oneflow/user/ops") != std::string::npos;
+                if (!is_cpp || is_in_database_dir)
+                    return;
+                if (!is_in_user_op_dir)
+                    return;
+                libclang_compile_config config(data.database, file);
+                std::ifstream           ifs(file);
+                std::string             content((std::istreambuf_iterator<char>(ifs)),
+                                    (std::istreambuf_iterator<char>()));
+                if (content.find("REGISTER_USER_OP") != std::string::npos)
+                {
+                    std::cerr << "found: " << file << "\n";
+                }
+                else
+                {
+                    std::cerr << "skip: " << file << "\n";
+                }
+                // data.parser.parse(std::move(file), std::move(config));
+            });
+            // cppast::parse_database(parser, database); // parse all files in the database
         }
         catch (cppast::libclang_error& ex)
         {
