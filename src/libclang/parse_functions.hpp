05bd2f4cafa63ac7bce1858993c459d324a12d5d
Jonathan Müller <git@foonathan.net>
2022-12-07 12:59:30 +0100

    Parse semantic parent of variable definitions
    Fixes #151.

--- src/libclang/parse_functions.hpp
+++ src/libclang/parse_functions.hpp
@@ -62,6 +62,8 @@ namespace detail
                                                         const parse_context& context,
                                                         const CXCursor& cur, const char* name);
 
+    type_safe::optional<cpp_entity_ref> get_semantic_parent(const CXCursor& cur, bool is_friend);
+
     std::unique_ptr<cpp_type> parse_type(const parse_context& context, const CXCursor& cur,
                                          const CXType& type);
 
@@ -91,8 +93,8 @@ namespace detail
     // unexposed
     std::unique_ptr<cpp_entity> try_parse_cpp_language_linkage(const parse_context& context,
                                                                const CXCursor&      cur);
-    
-    //unexposed
+
+    // unexposed
     std::unique_ptr<cpp_entity> try_parse_cpp_concept(const parse_context& context,
                                                       const CXCursor&      cur);
 
