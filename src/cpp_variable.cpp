12432de30d50c68a55e603c34d0b3a698c31cf22
Jonathan Müller <git@foonathan.net>
2022-12-07 12:47:42 +0100

    Add ability to set semanitc parent of a `cpp_variable`

--- src/cpp_variable.cpp
+++ src/cpp_variable.cpp
@@ -12,26 +12,26 @@ cpp_entity_kind cpp_variable::kind() noexcept
     return cpp_entity_kind::variable_t;
 }
 
-std::unique_ptr<cpp_variable> cpp_variable::build(const cpp_entity_index& idx, cpp_entity_id id,
-                                                  std::string name, std::unique_ptr<cpp_type> type,
-                                                  std::unique_ptr<cpp_expression> def,
-                                                  cpp_storage_class_specifiers    spec,
-                                                  bool                            is_constexpr)
+std::unique_ptr<cpp_variable> cpp_variable::build(
+    const cpp_entity_index& idx, cpp_entity_id id, std::string name, std::unique_ptr<cpp_type> type,
+    std::unique_ptr<cpp_expression> def, cpp_storage_class_specifiers spec, bool is_constexpr,
+    type_safe::optional<cpp_entity_ref> semantic_parent)
 {
     auto result = std::unique_ptr<cpp_variable>(
         new cpp_variable(std::move(name), std::move(type), std::move(def), spec, is_constexpr));
+    result->set_semantic_parent(std::move(semantic_parent));
     idx.register_definition(std::move(id), type_safe::cref(*result));
     return result;
 }
 
-std::unique_ptr<cpp_variable> cpp_variable::build_declaration(cpp_entity_id definition_id,
-                                                              std::string   name,
-                                                              std::unique_ptr<cpp_type>    type,
-                                                              cpp_storage_class_specifiers spec,
-                                                              bool is_constexpr)
+std::unique_ptr<cpp_variable> cpp_variable::build_declaration(
+    cpp_entity_id definition_id, std::string name, std::unique_ptr<cpp_type> type,
+    cpp_storage_class_specifiers spec, bool is_constexpr,
+    type_safe::optional<cpp_entity_ref> semantic_parent)
 {
     auto result = std::unique_ptr<cpp_variable>(
         new cpp_variable(std::move(name), std::move(type), nullptr, spec, is_constexpr));
+    result->set_semantic_parent(std::move(semantic_parent));
     result->mark_declaration(definition_id);
     return result;
 }
